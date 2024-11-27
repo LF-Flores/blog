948b20bfc2a72d00ccbb0d8ff0e9852a1f18117f
Felipe Flores <lffm.fismat@gmail.com>
2024-11-27 08:02:12 +0000

    [🔧config]: fix branch name

--- src/config.ts
+++ src/config.ts
@@ -13,7 +13,7 @@ export const SITE: Site = {
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
   showArchives: true,
   editPost: {
-    url: "https://github.com/LF-Flores/blog/edit/main/src/content/blog",
+    url: "https://github.com/LF-Flores/blog/edit/master/src/content/blog",
     text: "Suggest Changes",
     appendFilePath: true,
   },
